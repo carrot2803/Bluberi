7bafe9c83263a4651c415a279ebdbcb3ecbf5f90
carrot2803 <lezamadmitri@gmail.com>
2024-04-14 22:26:42 -0400

    MORE TYPES

--- App/models/Messages.py
+++ App/models/Messages.py
@@ -14,7 +14,7 @@ class ChatMessage(db.Model):
     created_at: str = db.Column(db.String(20), nullable=False)
 
     def __init__(self, sender_name, room_name, message, created_at) -> None:
-        self.sender_name = sender_name
-        self.room_name = room_name
-        self.message = message
-        self.created_at = created_at
+        self.sender_name: str = sender_name
+        self.room_name: str = room_name
+        self.message: str = message
+        self.created_at: str = created_at
