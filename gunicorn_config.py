e1213389384e469e727c5a6001eca39ecd6fe658
carrot2803 <lezamadmitri@gmail.com>
2024-04-19 19:34:19 -0400

    update ports

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,7 +3,7 @@ import multiprocessing
 
 # The socket to bind.
 # "0.0.0.0" to bind to all interfaces. 8000 is the port number.
-bind = "0.0.0.0:5000"
+bind = "0.0.0.0:8000"
 
 workers = 4
 worker_class = "gevent"
