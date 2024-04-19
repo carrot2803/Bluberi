70cecf25098b1611ef2a8b3c7d54f5916e528508
carrot2803 <lezamadmitri@gmail.com>
2024-04-19 18:16:42 -0400

    app

--- App/app.py
+++ App/app.py
@@ -42,18 +42,19 @@ def create_app(overrides={}) -> Flask:
 
 
 def load_config(app, overrides) -> None:
-    # hidden configs
-    app.config["ENV"] = "development"
+    # secret shud be inside a dot env
+    app.config["ENV"] = "production"
     app.config["JWT_SECRET_KEY"] = "jwt-secret-string"
-    app.config["SECRET_KEY"] = "felicia is an egg"
+    app.config["SECRET_KEY"] = "felicia is an"
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///database.db"
     app.config["PYTHON_VERSION"] = "3.11.3"
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=15)
+
     # general config
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_ACCESS_COOKIE_NAME"] = "access_token"
     app.config["JWT_REFRESH_COOKIE_NAME"] = "refresh_token"
     app.config["JWT_TOKEN_LOCATION"] = ["cookies", "headers"]
     app.config["JWT_HEADER_NAME"] = "Cookie"
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=15)
     app.config["JWT_COOKIE_CSRF_PROTECT"] = False
     app.config["JWT_COOKIE_SECURE"] = True
