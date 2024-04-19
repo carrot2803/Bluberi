49da74244b7eeb9fccf5308457ee652427e645b4
carrot2803 <lezamadmitri@gmail.com>
2024-04-19 19:32:37 -0400

    requirements update

---
+++ gunicorn_config.py
@@ -0,0 +1,15 @@
+# gunicorn_config.py
+import multiprocessing
+
+# The socket to bind.
+# "0.0.0.0" to bind to all interfaces. 8000 is the port number.
+bind = "0.0.0.0:5000"
+
+workers = 4
+worker_class = "gevent"
+# Log level
+loglevel = "info"
+
+# Where to log to
+accesslog = "-"  # '-' means log to stdout
+errorlog = "-"  # '-' means log to stderr
